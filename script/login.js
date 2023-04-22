4b9371afaa4c42cc490700bed6de3e9a15f1d93d
CA-WillMc <122403149+CA-WillMc@users.noreply.github.com>
2023-04-22 18:12:49 -0400

    ajout de la page login

---
+++ script/login.js
@@ -0,0 +1,6 @@
+function fermerLog() {
+  const loginPage = document.getElementById("wraper-login");
+  const formDisplay = document.querySelector("form");
+  formDisplay.style.display = "none";
+  loginPage.style.display = "none";
+}
