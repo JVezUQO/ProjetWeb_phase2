cb08c02a581db0b81eeddda2037f66e7a9b8e006
Lastjuggernaut <vezina_jean@outlook.com>
2023-04-23 15:42:24 -0400

    HTTP fonctionel !!!!

--- server/database.js
+++ server/database.js
@@ -4,10 +4,7 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const db = new sqlite3.Database('./server/test.db');
 
-let username;
-let password;
-let Publickey;
-let PrivateKey;
+
 
 app.get('/get', (req, res) => {
   db.all('SELECT * FROM EmailUser', (err, rows) => {
@@ -21,7 +18,7 @@ app.get('/get', (req, res) => {
 });
 
 app.get('/dummycreate', (req, res) => {
-    db.run('INSERT INTO Users(Name,Password,Publickey,PrivateKey) VALUES (?,?,?,?)',['Dummy_User','password','1234567890','qwerty'], (err, rows) => {
+    db.run('INSERT INTO Users(Name,Password,Publickey,Privatekey) VALUES (?,?,?,?)',['Dummy_User','password','1234567890','qwerty'], (err, rows) => {
       if (err) {
         console.error(err + ' Impossible de créer un utilisateur vide');
         res.status(500).send('');
@@ -32,7 +29,9 @@ app.get('/dummycreate', (req, res) => {
   });
 
   app.get('/usercreate', (req, res) => {
-    db.run('INSERT INTO Users(Name,Password,Publickey,PrivateKey) VALUES (?,?,?,?)',[username,password,Publickey,PrivateKey], (err, rows) => {
+    const {Name,Password,Publickey,Privatekey} = req.query;
+    db.run('INSERT INTO Users(Name,Password,Publickey,Privatekey) VALUES (?,?,?,?)',
+    [Name,Password,Publickey,Privatekey], (err, rows) => {
       if (err) {
         console.error(err + ' Impossible de créer un utilisateur vide');
         res.status(500).send('');
