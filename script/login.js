9049bed649a648f999c0da9e90c0e3d1255a641d
CA-WillMc <122403149+CA-WillMc@users.noreply.github.com>
2023-04-22 22:00:26 -0400

    login-verification user(debut)

--- script/login.js
+++ script/login.js
@@ -1,3 +1,38 @@
+const hashing = require("./hashing.js");
+function createKey() {
+  let pair = hashing.generateKeyPairRSA();
+  let public = pair.publicKey;
+  let private = pair.privateKey;
+  return public, private;
+}
+function verifierUser() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  /*if db includes username*/
+  if (true) {
+    /*password === db.username.password*/
+    if (true) {
+      fermerLog();
+    } else {
+      //message d'erreur
+      console.log(false);
+    }
+  } else {
+    /*creer un nouvel utilisateur dans db
+     {
+      username:
+      password:
+      publicKey:
+      privateKey:
+      email: []
+      contact:
+    }
+    */
+    createKey();
+    fermerLog();
+  }
+}
+
 function fermerLog() {
   const loginPage = document.getElementById("wraper-login");
   const formDisplay = document.querySelector("form");
