a1ed5d93d43cd9006712a22176b93d8015b25b21
CA-WillMc <122403149+CA-WillMc@users.noreply.github.com>
2023-04-23 19:03:31 -0400

    login temp

--- script/login.js
+++ script/login.js
@@ -1,38 +1,34 @@
 const hashing = require("./hashing.js");
-let x = false
+let x = false;
 function createKey() {
   let pair = hashing.generateKeyPairRSA();
   let public = pair.publicKey;
   let private = pair.privateKey;
   return public, private;
-
 }
 function verifierUser() {
- 
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
-  
-  fetchUsers(username,password)
-    
-  if (x == true){
+
+  fetchUsers(username, password);
+}
+/*
+  if (x == true) {
     fermerLog();
   } else {
-    alert('Password error, try again');
-   
+    alert("Password error, try again");
   }
-  
-  
+
   if (true) {
-    /*password === db.username.password*/
+    /*password === db.username.password
     if (true) {
       fermerLog();
-      
     } else {
       //message d'erreur
       console.log(false);
     }
   } else {
-    /*creer un nouvel utilisateur dans db
+    creer un nouvel utilisateur dans db
      {
       username:
       password:
@@ -41,38 +37,34 @@ function verifierUser() {
       email: []
       contact:
     }
-    */
-      console.log('nouvel utilisateur');
     
+    console.log("nouvel utilisateur");
+
     createKey();
     fermerLog();
   }
 }
-
+*/
 function fermerLog(x) {
-  if (x == true){
-  const loginPage = document.getElementById("wraper-login");
-  const formDisplay = document.querySelector("form");
-  formDisplay.style.display = "none";
-  loginPage.style.display = "none";
-  } else{
-    alert('Wrong password');
+  if (x == true) {
+    const loginPage = document.getElementById("wraper-login");
+    const formDisplay = document.querySelector("form");
+    formDisplay.style.display = "none";
+    loginPage.style.display = "none";
+  } else {
+    alert("Wrong password");
   }
 }
 
-
-
-function fetchUsers(username,password) {
+function fetchUsers(username, password) {
   fetch("http://localhost:3000/getUsers")
-    .then(response => response.text())
-    .then(data => JSON.parse(data).forEach(name => {
-      if (username === name.Name){
-          if (password === name.Password){
-            x == true;
-          }
-
+    .then((response) => response.text())
+    .then((data) =>
+      JSON.parse(data).forEach((name) => {
+        if (username === name.Name && password === name.Password) {
+          fermerLog(true);
         }
-    }))
-    .then (fermerLog(x))
-    .catch(error => console.error(error));
+      })
+    )
+    .catch((error) => console.error(error));
 }
