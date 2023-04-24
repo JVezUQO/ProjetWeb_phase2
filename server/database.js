ed0a84ba7257d9422e10ee7bb944b20544edb521
Lastjuggernaut <vezina_jean@outlook.com>
2023-04-23 22:05:47 -0400

    Comments in databse.js

--- server/database.js
+++ server/database.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json()); // parse application/json requests
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded requests
+
+//Va automatiquement créer la base de donné avec les tables qui sont nécessaires
+
 const db = new sqlite3.Database("./server/test.db");
 db.serialize(() => {
   db.serialize(() => {
@@ -31,9 +34,9 @@ db.serialize(() => {
   );
 
 
-//Sensé permettre au localhost de ce fetch a lui même, énorme problème de sécurité si sa devait être host...
+//Sensé permettre au localhost de ce fetch a lui même, énorme problème de sécurité si sa devait être host sur l'internet...
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*"); //Cette ligne en particulière est dangereuse...
   res.header(
     "Access-Control-Allow-Methods",
     "PUT, GET, POST, DELETE, PATCH, OPTIONS"
@@ -43,6 +46,7 @@ app.use((req, res, next) => {
   next();
 });
 
+//Commande qui va obtenir la liste de tout les users dans la table Users
 app.get("/getUsers", (req, res) => {
   db.all("SELECT * FROM Users", (err, rows) => {
     if (err) {
@@ -54,6 +58,7 @@ app.get("/getUsers", (req, res) => {
   });
 });
 
+//Commande qui va obtenir des emails de tout les users dans la table EmailUser
 app.get("/getEmail", (req, res) => {
   db.all("SELECT * FROM EmailUser", (err, rows) => {
     if (err) {
@@ -65,6 +70,7 @@ app.get("/getEmail", (req, res) => {
   });
 });
 
+//Va créer un user ''dummy'' avec des valeurs fixes 
 app.get("/dummycreate", (req, res) => {
   db.run(
     "INSERT INTO Users(Name,Password,Publickey,Privatekey) VALUES (?,?,?,?)",
@@ -80,6 +86,7 @@ app.get("/dummycreate", (req, res) => {
   );
 });
 
+//Va créer un user avec les paramêtres qu'il recoit
 app.post("/user", (req, res) => {
   const { name, password, publickey, privatekey } = req.body;
   console.log(req.body);
@@ -97,33 +104,8 @@ app.post("/user", (req, res) => {
   );
 });
 
-app.get("/spawn", (req, res) => {
-  db.serialize(() => {
-    db.serialize(() => {
-      db.run(
-        `CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Password TEXT, Publickey TEXT, Privatekey TEXT);`,
-        (err) => {
-          if (err) {
-            console.error(err.message);
-          } else {
-            console.log("Table User créer avec succès");
-          }
-        }
-      );
-      db.run(
-        `CREATE TABLE IF NOT EXISTS EmailUser (id INTEGER PRIMARY KEY AUTOINCREMENT, Titre TEXT, Destinataire TEXT, Message TEXT, Envoyeur TEXT );`,
-        (err) => {
-          if (err) {
-            console.error(err.message);
-          } else {
-            console.log("Table email créer avec succès");
-          }
-        }
-      );
-    });
-  });
-});
 
+//Ouvre le serveur au port 3000 pour /couter les commandes
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
